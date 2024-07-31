3cd86137cfd60e2a3218cd109e3dfd27b9772290
ShreeHarsha-22 <149221861+ShreeHarsha-22@users.noreply.github.com>
2024-07-31 16:35:29 +0530

    Add files via upload

---
+++ Week2 Design Patterns/src/Exercise3/src/Exercise3/Main.java
@@ -0,0 +1,27 @@
+package Exercise3;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Create a Computer with default settings
+        Computer basicComputer = new Computer.Builder()
+                .setCPU("Intel i5")
+                .setRAM("8GB")
+                .setStorage("256GB SSD")
+                .build();
+
+        // Create a Computer with additional options
+        Computer gamingComputer = new Computer.Builder()
+                .setCPU("Intel i9")
+                .setRAM("32GB")
+                .setStorage("1TB SSD")
+                .setGPU("NVIDIA RTX 3080")
+                .setOS("Windows 11")
+                .build();
+
+        // Print the details of both computers
+        System.out.println("Basic Computer: " + basicComputer);
+        System.out.println("Gaming Computer: " + gamingComputer);
+    }
+}
